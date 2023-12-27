3feba0a62224b494e424bfc5e1129b31852a0872
Fatih C <fatih@getcharteditor.com>
2023-12-27 15:46:35 -0800

    Veeran's changes

--- copaycard_roi_modeling.py
+++ copaycard_roi_modeling.py
@@ -1,75 +1,89 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
-st.set_page_config(page_title="Lamar Health ROI Calculator")
+st.set_page_config(page_title="Lamar Health ROI Calculator", layout="wide")
 
-st.markdown("<h1 style='text-align: center;'>Impact of copay accumulators and maximizers on manufacturer bottomline!</h1>", unsafe_allow_html=True)
-# Function to calculate gross and net revenue for each category
-def calculate_revenues(num_patients, price, manufacturer_copay, max_copay, num_cannot_afford):
-    # Placeholder for calculation logic
-    # Replace this with your actual calculation logic for each category
-    revenues = {
-        "Category": ["No accumulator/maximizer", "Accumulator", "Maximizer", "With Lamar Health"],
-        "Gross Revenue": [0, 0, 0, 0], 
-        "Manufacturer Costs": [0, 0, 0, 0],
-        "Net Revenue": [0, 0, 0, 0],    
-        "Lost Revenue": [0, 0, 0, 0] 
-    }
-    # Perform calculations here based on the inputs and update the values in 'revenues'
-    nothing_gross = num_patients*price
-    nothing_net = num_patients*(price-manufacturer_copay)
-    nothing_cost = nothing_gross - nothing_net
+st.markdown(
+    """
+    <div style='text-align: center;'>
+        <h1>Impact of Copay Accumulators on <br/> Manufacturer Bottomline!</h1>
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
 
-    accumulator_gross = (num_patients-num_cannot_afford)*price
-    accumulator_net = accumulator_gross-(num_patients-num_cannot_afford)*(manufacturer_copay)
-    accumulator_cost = accumulator_gross - accumulator_net
+df = pd.DataFrame(
+    [
+        {
+            "product": "Product A",
+            "avg_wholesale_price": 50000,
+            "insured_patients": 700,
+            "eligible_enrolled": 650,
+        },
+        {
+            "product": "Product B",
+            "avg_wholesale_price": 60000,
+            "insured_patients": 500,
+            "eligible_enrolled": 450,
+        },
+    ]
+)
 
-    maximizer_gross = (num_patients-num_cannot_afford)*price
-    maximizer_net = maximizer_gross-(num_patients-num_cannot_afford)*(max_copay)
-    maximizer_cost = maximizer_gross - maximizer_net
+# Calculate gross_annual_revenue for each product
+df["enrolled_copay_accumulator"] = (df["eligible_enrolled"] * 0.39).astype(int)
+df["likely_discontinue_therapy"] = (df["enrolled_copay_accumulator"] * 0.36).astype(int)
+df["gross_annual_revenue"] = df["insured_patients"] * df["avg_wholesale_price"]
+df["lost_revenue_discontinuation"] = df["avg_wholesale_price"] * df["likely_discontinue_therapy"]
 
-    lh_gross = num_patients*price
-    lh_net = num_patients*(price-manufacturer_copay)
-    lh_cost = lh_gross - lh_net
+st.data_editor(
+    df,
+    num_rows="dynamic",
+    hide_index=True,
+    column_config={
+        "product": st.column_config.Column(
+            "Product Name",
+        ),
+        "insured_patients": st.column_config.Column(
+            "Number of Patients",
+            help="The Number of Commercially Insured Patients on Therapy",
+        ),
+        "eligible_enrolled": st.column_config.Column(
+            "Patients with Copay Assistance",
+            help="The Number of Patients Eligible and Enrolled into Copay Assistance Program",
+        ),
+        "avg_wholesale_price": st.column_config.NumberColumn(
+            "Price of Product",
+            help="Wholesale Acquisition Cost (WAC) of the Therapy",
+            format="$%d",
+        ),
+        "gross_annual_revenue": st.column_config.NumberColumn(
+            "Gross Annual Revenue",
+            format="$%d",
+        ),
+        "enrolled_copay_accumulator": st.column_config.Column(
+            "Number of Patients in Accumulator Programs",
+            help="In 2022, 39 percent of all commercially covered lives are enrolled into accumulator programs: \
+            https://www.drugchannels.net/2023/02/copay-accumulator-and-maximizer-update \
+            ",
+        ),
+        "likely_discontinue_therapy": st.column_config.Column(
+            "Number of Patients Likely to Discontinue",
+            help="36 percent of patients that face a copay accumulator are likely to discontinue therapy: \
+            https://phrma.org/-/media/Project/PhRMA/PhRMA-Org/PhRMA-Org/PDF/A-C/Accumulator-adjustment-programs-from-payers-lead-to-surprise-out-of-pocket-costs-and-nonadherence.pdf",
+        ),
+        "lost_revenue_discontinuation": st.column_config.NumberColumn(
+            "Lost Revenue",
+            help="The amount of revenue lost to manufacturer due to discontinuation.",
+            format="$%d",
+        ),
+    },
+)
 
-    revenues["Gross Revenue"] = [nothing_gross, accumulator_gross, maximizer_gross, lh_gross]
-    revenues["Manufacturer Costs"] = [nothing_cost, accumulator_cost, maximizer_cost, lh_cost]
-    revenues["Net Revenue"] = [nothing_net, accumulator_net, maximizer_net, lh_net]
-    revenues["Lost Revenue"] = [nothing_net-nothing_net, nothing_net-accumulator_net, nothing_net-maximizer_net, nothing_net-lh_net]
-
-    return pd.DataFrame(revenues)
-
-# Main function for the Streamlit app
-def main():
-
-    # Input fields
-    with st.form("input_form"):
-        num_patients = st.number_input("Number of Patients", value=1000)
-        price = st.number_input("Price of the Product", value = 24000)
-        manufacturer_copay = st.number_input("Manufacturer Copay (based on patient out-of-pocket costs, deductible)", value=6000)
-        max_copay = st.number_input("Maximum Copay", value=16000)
-        num_cannot_afford = st.number_input("Number of Patients Who Can't Afford Out-of-Pocket Costs", value=100)
-
-        submit_button = st.form_submit_button("Calculate Revenues", type="primary")
-
-        if submit_button:
-            # Perform calculations
-            revenue_df = calculate_revenues(num_patients, price, manufacturer_copay, max_copay, num_cannot_afford)
-    
-            # Format the numbers in the DataFrame
-            formatted_df = revenue_df.copy()
-            formatted_df["Gross Revenue"] = formatted_df["Gross Revenue"].apply(lambda x: f"${x:,.0f}")
-            formatted_df["Manufacturer Costs"] = formatted_df["Manufacturer Costs"].apply(lambda x: f"${x:,.0f}")
-            formatted_df["Net Revenue"] = formatted_df["Net Revenue"].apply(lambda x: f"${x:,.0f}")
-            formatted_df["Lost Revenue"] = formatted_df["Lost Revenue"].apply(lambda x: f"${x:,.0f}")
-
-            # Reset and drop the index
-            # formatted_df.reset_index(drop=True, inplace=True)
-            
-            # Display the DataFrame using st.dataframe
-            st.dataframe(formatted_df.style.highlight_between(left="$0", inclusive="neither", subset=["Lost Revenue"], color="red"));
-
-if __name__ == "__main__":
-    main()
+st.header("Resources:")
+st.image("rise_of_accumulators.jpeg")
+st.image("likely_to_discontinue.png")
+st.markdown("""
+    - [Rise of Copay Accumulators and Maximizers](https://www.drugchannels.net/2023/02/copay-accumulator-and-maximizer-update.html)
+    - [Copay Accumulator Effects on Patient Adherence](https://phrma.org/-/media/Project/PhRMA/PhRMA-Org/PhRMA-Org/PDF/A-C/Accumulator-adjustment-programs-from-payers-lead-to-surprise-out-of-pocket-costs-and-nonadherence.pdf)
+    - [J&J lawsuit against copay maximizers for $100 million dollars](https://www.example.com)
+""")
